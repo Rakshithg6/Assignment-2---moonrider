4a272e4bfc313ac6d96a1a7a3fcfd4cfb40340d5
Rakshith G <rakshithg6@users.noreply.github.com>
2025-06-24 18:12:36 +0530

    Initial commit

--- src/main/java/com/javatechie/crud/example/entity/Product.java
+++ src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -22,4 +22,32 @@ public class Product {
     private String name;
     private int quantity;
     private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
